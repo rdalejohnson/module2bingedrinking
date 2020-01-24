5f62856dda43a05ce4d5d1b8fbeef82c5a68b06e
Johnson <rdjohnson@ad.hs.uab.edu>
2020-01-24 15:25:25 -0600

    Good demo of NA problems

--- module2HomeworkOFFICIALSOLUTION.R
+++ module2HomeworkOFFICIALSOLUTION.R
@@ -1,72 +1,71 @@
 # title: "BST 611 SAS Lab for  Regression "
 # author: "BST 611"
 
-# set working directory 
-#setwd("D:/FuchenchuWang/UAB/BST 611/conversion")
-
 # read your file
 lab=read.csv("Binge_Drinking.csv")
 
-# Split the dataset by Gender 
-# 1 = Male
-# 2 = Female""
 
 # change "1/2" to "Male/Female"
 lab$gender = gsub(1, "Male",lab$gender)
 lab$gender = gsub(2, "Female",lab$gender)
 
-# subset a dataset only contains male
-male_with_NA_excluded <- lab[lab$gender=="Male" & !is.na(lab$gender), ]
+#Six rows in our binge drinking dataset have a NA for gender
+#Sort by gender and then look at the last few rows to see
+#the NA values in the gender column:
+print(tail(lab[order(lab$gender),], n=10))
+
+#and six rows have a NA age value:
+print(tail(lab[order(lab$age),], n=10))
+
+#the subset operation will take the NA gender when you ask for MALES only
+#for some reason
 male <- lab[lab$gender=="Male", ]
+print(tail(male[order(male$gender),], n=10))
 
-female <- lab[lab$gender=="Female" , ]
+#and the same goes for FEMALE:
+
+female <- lab[lab$gender=="Female", ]
+print(tail(female[order(female$gender),], n=10))
+
+#And note that in the rows at the bottom of both sets that
+#we just printed, ALL the values in the row have suddenly
+#become NAs.  Just weird
+
+
+# So, to subset a dataset only contains male and truly omits the NA values, use
+# an extra condition
+male_with_NA_excluded <- lab[lab$gender=="Male" & !is.na(lab$gender), ]
+print(tail(male_with_NA_excluded[order(male_with_NA_excluded$gender),], n=10))
+
 female_with_NA_excluded <- lab[lab$gender=="Female" & !is.na(lab$gender),]
+print(tail(female_with_NA_excluded[order(female_with_NA_excluded$gender),], n=10))
 
-sum(is.na(male$age))
 
-sum(is.na(female$age))
+#you can use this set of commands to count the NAs in a column:
 
-sum(is.na(lab$age))
+sum(is.na(male$gender))
+sum(is.na(female$gender))
 
-sum(is.na(lab$gender))
+sum(is.na(male_with_NA_excluded$gender))
+sum(is.na(female_with_NA_excluded$gender))
+
+#And you can remove entire rows that have any NAs in them with this:
+labs_with_no_NA_anywhere <- na.omit(lab)
+sum(is.na(labs_with_no_NA_anywhere$gender))
+print(labs_with_no_NA_anywhere)
+
 
 
 summary(lab$age)
 
 summary(male$age)
-hist(male$age)
 
 summary(female$age)
-hist(female$age)
 
 summary(male_with_NA_excluded$age)
-hist(male_with_NA_excluded$age)
 
 summary(female_with_NA_excluded$age)
-hist(female_with_NA_excluded$age)
 
-# Recode the continuous variable, Age, into three groups:  < 20 years; 20-25 years; and >25 years
-age_in_year= ifelse(lab$age<=19,1,
-                    ifelse(lab$age>19 & lab$age<=25, "2","3"
-                    ))
-lab_new=cbind(lab,age_in_year)
-table(lab_new$age_in_year)
+summary(labs_with_no_NA_anywhere$age)
 
 
-# Figure
-lab$drinks5 = gsub(1, "None",lab$drinks5)
-lab$drinks5 = gsub(2, "Once",lab$drinks5)
-lab$drinks5 = gsub(3, "Twice",lab$drinks5)
-lab$drinks5 = gsub(4, "three to five",lab$drinks5)
-lab$drinks5 = gsub(5, "six to nine",lab$drinks5)
-lab$drinks5 = gsub(6, "more than ten",lab$drinks5)
-
-# give x label a order, otherwise it will show in alphabet
-x.order=c("None","Once", "Twice","three to five","six to nine","more than ten")
-lab$drinks5 = factor(lab$drinks5, levels=unique(x.order))
-
-figure=cbind(lab[,c(1,2,6)])
-figure.delete=na.omit(figure)
-
-library(ggplot2)
-plot(ggplot(figure.delete, aes(x=drinks5,y=age,color=gender,group=gender)) + geom_smooth(method="loess",se=F)+labs(x="Binge Drinking",y="Age (years)",title="Age and Binge Drinking by Gender"))
