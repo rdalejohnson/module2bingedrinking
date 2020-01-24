5901e9fc7c1dcdaa92c7cc89ca7cb1e543356152
Robert Johnson <robertjohnson@Roberts-Mini.attlocal.net>
2020-01-24 03:20:45 -0600

    Adding summary, NA tests.  Adding the official solution to the project for comparison purposes

--- module2Homework.R
+++ module2Homework.R
@@ -1,7 +1,7 @@
 #R. Dale Johnson
 #Module 2 Homework - binge drinking data set
 
-bingeDrinking <- read.csv(file="Binge_Drinking.csv", header=TRUE, sep=",")
+bingeDrinking <- read.csv(file="Binge_Drinking.csv"), header=TRUE, sep=",")
 
 bingeDrinking
 
@@ -9,7 +9,7 @@ bingeDrinking
 ########################
 head(bingeDrinking)
 print(nrow(bingeDrinking)) #gets you number of rows only
-print(ncol(bingeDrinking)) #gets you number of rows only
+print(ncol(bingeDrinking)) #gets you number of columns only
 print(dim(bingeDrinking))  #gets you rows and columns - the dimension
 print(colnames(bingeDrinking))
 
@@ -17,9 +17,10 @@ print(colnames(bingeDrinking))
 names(bingeDrinking)
 
 ###############################
-# Source: https://stackoverflow.com/a/19327079/3500818
+# Source for using split: https://stackoverflow.com/a/19327079/3500818
+###
 
-mylist <- split(bingeDrinking, bingeDrinking$gender)
+mylist <- split(bingeDrinking, drop=FALSE , bingeDrinking$gender)
 
 malesOnly <- mylist$'1'
 
@@ -29,3 +30,23 @@ malesOnly
 
 femalesOnly
 
+####NA counting ###########
+#Source: https://stackoverflow.com/a/24027625/3500818
+###
+
+sum(is.na(malesOnly$age))
+
+sum(is.na(femalesOnly$age))
+
+sum(is.na(bingeDrinking$age))
+
+
+
+
+############### Frequency distribution #################
+
+summary(bingeDrinking$age)
+
+summary(malesOnly$age)
+
+summary(femalesOnly$age)
---
+++ module2HomeworkOFFICIALSOLUTION.R
@@ -0,0 +1,54 @@
+# title: "BST 611 SAS Lab for  Regression "
+# author: "BST 611"
+
+# set working directory 
+#setwd("D:/FuchenchuWang/UAB/BST 611/conversion")
+
+# read your file
+lab=read.csv("Binge_Drinking.csv")
+
+# Split the dataset by Gender 
+# 1 = Male
+# 2 = Female""
+
+# change "1/2" to "Male/Female"
+lab$gender = gsub(1, "Male",lab$gender)
+lab$gender = gsub(2, "Female",lab$gender)
+
+# subset a dataset only contains male
+male=lab[lab$gender=="Male",]
+female=lab[lab$gender=="Female",]
+
+summary(lab$age)
+
+summary(male$age)
+hist(male$age)
+
+summary(female$age)
+hist(female$age)
+
+# Recode the continuous variable, Age, into three groups:  < 20 years; 20-25 years; and >25 years
+age_in_year= ifelse(lab$age<=19,1,
+                    ifelse(lab$age>19 & lab$age<=25, "2","3"
+                    ))
+lab_new=cbind(lab,age_in_year)
+table(lab_new$age_in_year)
+
+
+# Figure
+lab$drinks5 = gsub(1, "None",lab$drinks5)
+lab$drinks5 = gsub(2, "Once",lab$drinks5)
+lab$drinks5 = gsub(3, "Twice",lab$drinks5)
+lab$drinks5 = gsub(4, "three to five",lab$drinks5)
+lab$drinks5 = gsub(5, "six to nine",lab$drinks5)
+lab$drinks5 = gsub(6, "more than ten",lab$drinks5)
+
+# give x label a order, otherwise it will show in alphabet
+x.order=c("None","Once", "Twice","three to five","six to nine","more than ten")
+lab$drinks5 = factor(lab$drinks5, levels=unique(x.order))
+
+figure=cbind(lab[,c(1,2,6)])
+figure.delete=na.omit(figure)
+
+library(ggplot2)
+plot(ggplot(figure.delete, aes(x=drinks5,y=age,color=gender,group=gender)) + geom_smooth(method="loess",se=F)+labs(x="Binge Drinking",y="Age (years)",title="Age and Binge Drinking by Gender"))
